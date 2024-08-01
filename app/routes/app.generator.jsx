b3e5625fe258621793514c32f0094b7f3334a98a
Piyush Bhatia <Piyush_Bhatia@epam.com>
2024-08-01 23:02:14 +0530

    Correcting changes

--- app/routes/app.generator.jsx
+++ app/routes/app.generator.jsx
@@ -265,11 +265,6 @@ export default function GeneratorComponent() {
           {products.length==0 ? 
           <EmptyState
           heading="No Products Found"
-          action={{content: 'Add Products'}}
-          secondaryAction={{
-            content: 'Learn more',
-            url: 'https://help.shopify.com',
-          }}
           image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
         >
           <p>Track and receive your incoming inventory from suppliers.</p>
